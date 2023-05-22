caa920a18bb61e90394ba1544823e57b672d33f9
Danilo Oliveira <doliveira100720@gmail.com>
2023-05-22 08:29:16 -0300

    user - adicionado seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}

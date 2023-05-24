7a470efc4599aa5caaba6d49bc409865333bf749
Danilo Oliveira <doliveira100720@gmail.com>
2023-05-24 08:39:57 -0300

    adicionado laravel ekko

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Laravelista\Ekko\Frameworks\Laravel\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -184,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Ekko' => Laravelista\Ekko\Frameworks\Laravel\Facade::class,
     ])->toArray(),
 
 ];

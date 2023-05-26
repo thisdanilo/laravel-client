2ab070be9eb69d6c06187b0d585a3a2a33b72d7c
Danilo Oliveira <doliveira100720@gmail.com>
2023-05-26 09:20:38 -0300

    adicionado assets

---
+++ resources/js/client.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    //-----------------------------------------------------
+    // Defining a variable
+    //-----------------------------------------------------
+
+    var token = $("input[name='_token']").val(),
+        datatable_url = window.location.origin + "/datatable/pt-br.json";
+
+    //-----------------------------------------------------
+    // Instance of plugins
+    //-----------------------------------------------------
+
+    $("#ajax-datatable").DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: $("#route_datatable").val(),
+            type: "POST",
+            beforeSend: function (request) {
+                return request.setRequestHeader("X-CSRF-Token", token);
+            },
+        },
+        columns: [
+            { data: "id" },
+            { data: "name" },
+            { data: "cpf" },
+            { data: "email" },
+            { data: "active" },
+            { data: "action", orderable: false, searchable: false },
+        ],
+        language: {
+            url: datatable_url,
+        },
+    });
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,12 +15,28 @@ const mix = require('laravel-mix');
  mix.combine([
     "node_modules/admin-lte/plugins/fontawesome-free/css/all.css",
     "node_modules/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css",
+    "node_modules/admin-lte/plugins/datatables-bs4/css/dataTables.bootstrap4.css",
+    "node_modules/admin-lte/plugins/datatables-responsive/css/responsive.bootstrap4.css",
+    "node_modules/admin-lte/plugins/datatables-buttons/css/buttons.bootstrap4.css",
     "node_modules/admin-lte/dist/css/adminlte.min.css",
+    "resources/css/style.css",
 ], "public/css/admin.css");
 
  /* Admin JS */
  mix.combine([
     "node_modules/admin-lte/plugins/jquery/jquery.min.js",
     "node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js",
+    "node_modules/admin-lte/plugins/datatables/jquery.dataTables.js",
+    "node_modules/admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.js",
+    "node_modules/admin-lte/plugins/datatables-responsive/js/dataTables.responsive.js",
+    "node_modules/admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.js",
+    "node_modules/admin-lte/plugins/datatables-buttons/js/dataTables.buttons.js",
+    "node_modules/admin-lte/plugins/datatables-buttons/js/buttons.bootstrap4.js",
     "node_modules/admin-lte/dist/js/adminlte.min.js",
 ], "public/js/admin.js");
+
+// Client JS
+mix.scripts(
+    "resources/js/client.js",
+    "public/js/client.js"
+);

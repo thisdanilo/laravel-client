4e49a234198f898b341f3be2617e374bb7f6ba18
Danilo Oliveira <doliveira100720@gmail.com>
2023-05-26 09:21:05 -0300

    client - adicionado model e migration

---
+++ app/Models/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Client extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'clients';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'cpf',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
